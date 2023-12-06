bcec57e9e71707b29c0d6beb41b18b0e5434fe02
bruce-mig <h180009t@hit.ac.zw>
2023-12-06 16:05:25 +0200

    api handlers

---
+++ api/server.go
@@ -0,0 +1,38 @@
+package api
+
+import (
+	db "github.com/bruce-mig/simple-bank/db/sqlc"
+	"github.com/gin-gonic/gin"
+)
+
+// Server serves HTTP requests for out banking service.
+type Server struct {
+	store  *db.Store
+	router *gin.Engine
+}
+
+// NewServer creates a new HTTP server and setup routing
+func NewServer(store *db.Store) *Server {
+	server := &Server{
+		store: store,
+	}
+	router := gin.Default()
+
+	router.POST("/accounts", server.createAccount)
+	router.GET("/accounts/:id", server.getAccount)
+	router.GET("/accounts", server.listAccounts)
+	router.PUT("/accounts/:id", server.updateAccount)
+	router.DELETE("/accounts/:id", server.deleteAccount)
+
+	server.router = router
+	return server
+}
+
+// Start runs the HTTP server ona specific address.
+func (server *Server) Start(address string) error {
+	return server.router.Run(address)
+}
+
+func errorResponse(err error) gin.H {
+	return gin.H{"error": err.Error()}
+}
