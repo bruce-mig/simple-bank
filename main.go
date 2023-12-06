04f182379b4da340c7e4e604b3ed6149b0511ce2
bruce-mig <h180009t@hit.ac.zw>
2023-12-06 16:04:07 +0200

    setup db connection

---
+++ main.go
@@ -0,0 +1,31 @@
+package main
+
+import (
+	"database/sql"
+	"log"
+
+	"github.com/bruce-mig/simple-bank/api"
+	db "github.com/bruce-mig/simple-bank/db/sqlc"
+	"github.com/bruce-mig/simple-bank/util"
+	_ "github.com/lib/pq"
+)
+
+func main() {
+	config, err := util.LoadConfig(".")
+	if err != nil {
+		log.Fatal("cannot load config:", err)
+	}
+	conn, err := sql.Open(config.DBDriver, config.DBSource)
+	if err != nil {
+		log.Fatal("cannot connect to db", err)
+	}
+
+	store := db.NewStore(conn)
+	server := api.NewServer(store)
+
+	err = server.Start(config.ServerAddress)
+	if err != nil {
+		log.Fatal("cannot start server", err)
+	}
+
+}
