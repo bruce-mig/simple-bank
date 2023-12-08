925dffd26a1f6e7ef629581f3470ba490bc0e769
bruce-mig <h180009t@hit.ac.zw>
2023-12-08 15:17:21 +0200

    user implementation

--- util/random.go
+++ util/random.go
@@ -1,6 +1,7 @@
 package util
 
 import (
+	"fmt"
 	"math/rand"
 	"strings"
 	"time"
@@ -44,4 +45,9 @@ func RandomCurrency() string {
 	currencies := []string{ZAR, USD, BWP}
 	n := len(currencies)
 	return currencies[rand.Intn(n)]
+}
+
+// RandomEmail generates a random email
+func RandomEmail() string {
+	return fmt.Sprintf("%s@email.com", RandomString(6))
 }
