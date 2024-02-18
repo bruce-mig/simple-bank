c944dbdd3dbd3ac4208737fae7456dc6e7e81295
bruce-mig <h180009t@hit.ac.zw>
2024-02-18 15:29:56 +0200

    login user grpc API unit test

---
+++ gapi/rpc_login_user_test.go
@@ -0,0 +1,141 @@
+package gapi
+
+import (
+	"context"
+	"database/sql"
+	"testing"
+
+	mockdb "github.com/bruce-mig/simple-bank/db/mock"
+	db "github.com/bruce-mig/simple-bank/db/sqlc"
+	"github.com/bruce-mig/simple-bank/pb"
+	"github.com/bruce-mig/simple-bank/util"
+	"github.com/stretchr/testify/require"
+	"go.uber.org/mock/gomock"
+	"google.golang.org/grpc/codes"
+	"google.golang.org/grpc/status"
+)
+
+func TestLoginUserAPI(t *testing.T) {
+	user, password := randomUser(t, util.DepositorRole)
+
+	testCases := []struct {
+		name          string
+		req           *pb.LoginUserRequest
+		buildStubs    func(store *mockdb.MockStore)
+		checkResponse func(t *testing.T, res *pb.LoginUserResponse, err error)
+	}{
+		{
+			name: "OK",
+			req: &pb.LoginUserRequest{
+				Username: user.Username,
+				Password: password,
+			},
+			buildStubs: func(store *mockdb.MockStore) {
+				store.EXPECT().
+					GetUser(gomock.Any(), gomock.Eq(user.Username)).
+					Times(1).
+					Return(user, nil)
+				store.EXPECT().
+					CreateSession(gomock.Any(), gomock.Any()).
+					Times(1)
+			},
+			checkResponse: func(t *testing.T, res *pb.LoginUserResponse, err error) {
+				require.NoError(t, err)
+				require.NotNil(t, res)
+				require.Equal(t, user.Username, res.User.Username)
+				require.Equal(t, user.FullName, res.User.FullName)
+				require.Equal(t, user.Email, res.User.Email)
+				st, ok := status.FromError(err)
+				require.True(t, ok)
+				require.Equal(t, codes.OK, st.Code())
+			},
+		},
+		{
+			name: "UserNotFound",
+			req: &pb.LoginUserRequest{
+				Username: user.Username,
+				Password: password,
+			},
+			buildStubs: func(store *mockdb.MockStore) {
+				store.EXPECT().
+					GetUser(gomock.Any(), gomock.Any()).
+					Times(1).
+					Return(db.User{}, db.ErrRecordNotFound)
+			},
+			checkResponse: func(t *testing.T, res *pb.LoginUserResponse, err error) {
+				st, ok := status.FromError(err)
+				require.True(t, ok)
+				require.Equal(t, codes.NotFound, st.Code())
+			},
+		},
+		{
+			name: "IncorrectPassword",
+			req: &pb.LoginUserRequest{
+				Username: user.Username,
+				Password: "incorrect",
+			},
+			buildStubs: func(store *mockdb.MockStore) {
+				store.EXPECT().
+					GetUser(gomock.Any(), gomock.Eq(user.Username)).
+					Times(1).
+					Return(user, nil)
+			},
+			checkResponse: func(t *testing.T, res *pb.LoginUserResponse, err error) {
+				st, ok := status.FromError(err)
+				require.True(t, ok)
+				require.Equal(t, codes.NotFound, st.Code())
+			},
+		},
+		{
+			name: "InternalError",
+			req: &pb.LoginUserRequest{
+				Username: user.Username,
+				Password: password,
+			},
+			buildStubs: func(store *mockdb.MockStore) {
+				store.EXPECT().
+					GetUser(gomock.Any(), gomock.Any()).
+					Times(1).
+					Return(db.User{}, sql.ErrConnDone)
+			},
+			checkResponse: func(t *testing.T, res *pb.LoginUserResponse, err error) {
+				st, ok := status.FromError(err)
+				require.True(t, ok)
+				require.Equal(t, codes.Internal, st.Code())
+			},
+		},
+		{
+			name: "InvalidUsername",
+			req: &pb.LoginUserRequest{
+				Username: "invalid-user#1",
+				Password: password,
+			},
+			buildStubs: func(store *mockdb.MockStore) {
+				store.EXPECT().
+					GetUser(gomock.Any(), gomock.Any()).
+					Times(0)
+			},
+			checkResponse: func(t *testing.T, res *pb.LoginUserResponse, err error) {
+				st, ok := status.FromError(err)
+				require.True(t, ok)
+				require.NotEqual(t, codes.OK, st.Code())
+			},
+		},
+	}
+
+	for i := range testCases {
+		tc := testCases[i]
+
+		t.Run(tc.name, func(t *testing.T) {
+			storeCtrl := gomock.NewController(t)
+			defer storeCtrl.Finish()
+			store := mockdb.NewMockStore(storeCtrl)
+
+			tc.buildStubs(store)
+			server := newTestServer(t, store, nil)
+
+			res, err := server.LoginUser(context.Background(), tc.req)
+			tc.checkResponse(t, res, err)
+		})
+	}
+}
